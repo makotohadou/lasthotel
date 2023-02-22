008eb806edd10da513b14497d05fcd6929c5d331
Gaspar Monteiro <gaspar.silva@gmail.com>
2023-02-22 12:47:20 -0300

    Swagger support.

--- src/main/java/com/cancun/lasthotel/reservation/ReservationApplication.java
+++ src/main/java/com/cancun/lasthotel/reservation/ReservationApplication.java
@@ -2,15 +2,33 @@ package com.cancun.lasthotel.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class ReservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReservationApplication.class, args);
+    }
+
+    @Configuration
+    public class SpringFoxConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+    }
 
 }
